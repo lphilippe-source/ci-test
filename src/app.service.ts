df8f9b0bcaa47dcc7309b861231000eeb835c7da
Philippe <philippe.3.lemaire@atos.net>
2023-05-11 09:24:39 +0200

    fix: controller

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(x,y): number {
-    return x+y
+  getHello(): string {
+    return "Hello World"
   }
 }
